c249140d80f72a0ace1e07e749a67bd9f4d21785
Samson <57755235+Samson-DVS@users.noreply.github.com>
2021-04-23 13:26:33 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 23 11:29:10 2021
+
+@author: samson
+"""
+#Install the libraries first using anaconda prompt
+#pip install flask
+#pip install twilio
+
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    msg = request.form.get('Body')
+
+    # Create reply
+    resp = MessagingResponse()
+    resp.message("You said: {}".format(msg))
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
